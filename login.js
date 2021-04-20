b9d000f37293df191267dfcdb7b1be7fe1d719be
2007akshay <72021287+2007akshay@users.noreply.github.com>
2021-04-20 11:59:03 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,23 @@
+
+function validate (){
+if (document.getElementById("username").value==="007" && document.getElementById("password").value==="007"  )
+  {
+    alert("login Done 😊")
+    location.href="login.html"
+  }
+  else if (document.getElementById("username").value==="008" && document.getElementById("password").value==="008" ){
+    alert("login Done 😊 ")
+    location.href="login.html"
+  }
+  else if (document.getElementById("username").value==="ashwa" && document.getElementById("password").value==="9449001728" ){
+    alert("login Done 😊")
+    location.href="login.html"
+  }
+  else if (document.getElementById("username").value==="sujal" && document.getElementById("password").value==="sujal" ){
+    alert("login Done 😊")
+    location.href="login.html"
+  }
+  else {
+    alert("login failed 😔")
+  }
+}
